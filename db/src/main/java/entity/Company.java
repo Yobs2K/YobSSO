7ba165bee6c3127eb381d3b9014921110319f4e4
Deniz Yolsal <46849613+Yobs2K@users.noreply.github.com>
2023-01-31 14:56:53 +0300

    entities added

--- db/src/main/java/entity/Company.java
+++ db/src/main/java/entity/Company.java
@@ -1,8 +1,54 @@
 package entity;
 
-import javax.persistence.Entity;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
+@Table(name = "company")
 public class Company extends AbstractBaseEntity<Long>{
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "company_user",
+        joinColumns = @JoinColumn(name = "company_id"),
+        inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> users = new HashSet<>();
+
+    @OneToMany(
+            mappedBy = "company",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Group> groups = new ArrayList<>();
+
+    public void addUser(User user) {
+        users.add(user);
+        user.getCompanies().add(this);
+    }
+
+    public void removeUser(User user) {
+        users.remove(user);
+        user.getCompanies().remove(this);
+    }
+
+    public void addGroup(Group group) {
+        groups.add(group);
+        group.setCompany(this);
+    }
+
+    public void removeGroup(Group group) {
+        groups.remove(group);
+        group.setCompany(null);
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public List<Group> getGroups() {
+        return groups;
+    }
 }
--- db/src/main/java/entity/User.java
+++ db/src/main/java/entity/User.java
@@ -1,9 +1,24 @@
 package entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
+@Table(name = "user")
 public class User extends AbstractBaseEntity<Long> {
 
+    @ManyToMany(mappedBy = "users")
+    private Set<Company> companies = new HashSet<>();
+
+    @ManyToMany(mappedBy = "users")
+    private Set<Group> groups = new HashSet<>();
+
+    public Set<Company> getCompanies() {
+        return companies;
+    }
+
+    public Set<Group> getGroups() {
+        return groups;
+    }
 }
