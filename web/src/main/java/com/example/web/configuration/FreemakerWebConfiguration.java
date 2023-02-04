f4e080288030f650c14953bdb21eb947c670e897
alexey menshov <aleks7777k2@gmail.com>
2023-02-05 02:07:03 +0300

    add freemarker config and base auth tempalate

---
+++ web/src/main/java/com/example/web/configuration/FreemakerWebConfiguration.java
@@ -0,0 +1,33 @@
+package com.example.web.configuration;
+
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.cache.TemplateLoader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactory;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@Configuration
+public class FreemakerWebConfiguration {
+
+
+    @Bean
+    public FreeMarkerViewResolver freemarkerViewResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setCache(true);
+        resolver.setSuffix(".ftlh");
+        return resolver;
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freemarkerConfig() {
+        FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+        freeMarkerConfigurer.setTemplateLoaderPath("classpath:/templates/");
+        return freeMarkerConfigurer;
+    }
+
+}
---
+++ web/src/main/java/com/example/web/configuration/WebConfiguration.java
@@ -0,0 +1,11 @@
+package com.example.web.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+}
