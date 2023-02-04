1049a9d93147e06fbf7989bc41d7a3ee928ea3d5
alexey menshov <aleks7777k2@gmail.com>
2023-02-04 20:00:41 +0300

    switch to another branch

--- web/src/main/java/com/example/web/configuration/WebSecurityConfiguration.java
+++ web/src/main/java/com/example/web/configuration/WebSecurityConfiguration.java
@@ -1,17 +1,17 @@
 package com.example.web.configuration;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@ComponentScan(value = {
-        "com.example.db",
-        "com.example.core",
-})
-public class WebApplicationConfiguration {
+@EnableWebSecurity
+public class WebSecurityConfiguration {
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
