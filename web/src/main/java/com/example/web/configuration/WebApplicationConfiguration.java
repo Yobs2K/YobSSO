58a1420f6ee5eccbc0ac899f39c03a5738df08c0
alexey menshov <aleks7777k2@gmail.com>
2023-02-05 17:50:39 +0300

    small fixes

--- web/src/main/java/com/example/web/configuration/WebApplicationConfiguration.java
+++ web/src/main/java/com/example/web/configuration/WebApplicationConfiguration.java
@@ -4,8 +4,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.example.db.repository")
@@ -14,22 +12,5 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
         "com.example.db",
         "com.example.core",
 })
-public class WebApplicationConfiguration extends WebSecurityConfigurerAdapter {
-
-
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http
-                .authorizeRequests()
-                .antMatchers("/**").permitAll();
-
-
-        http.formLogin()
-                .loginPage("/auth/login")
-                .usernameParameter("email")
-                .passwordParameter("password")
-                .permitAll();
-
-
-    }
+public class WebApplicationConfiguration {
 }
--- web/src/main/java/com/example/web/configuration/WebSecurityConfiguration.java
+++ web/src/main/java/com/example/web/configuration/WebSecurityConfiguration.java
@@ -2,16 +2,35 @@ package com.example.web.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfiguration {
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests()
+                .antMatchers("/**").permitAll();
+
+
+        http.formLogin()
+                .loginPage("/auth/login")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .permitAll();
+
+
+    }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
